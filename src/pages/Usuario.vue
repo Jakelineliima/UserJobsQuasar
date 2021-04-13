d08a5df542e79329cb2e726549cff064f9d4fc01
Jakeline Liima <jakeline.liima555@gmail.com>
2021-04-12 22:11:37 -0300

    pag usuario

--- src/pages/Usuario.vue
+++ src/pages/Usuario.vue
@@ -3,32 +3,36 @@
      <q-card class="my-card">
       <q-card-section>
         <div class="text-h6 text-primary"  style="margin-bottom: 1em">Ester Nundes Line</div>
-        <p>Endereço: Rua Jose Tutyli N° 120 Jardim Paumas Itápolis</p>
-        <p>Escolaridade: Ensino Superior incompleto</p>
+        <p><strong>Endereço : </strong> Rua Jose Tutyli  N° 120 Jardim Paumas Itápolis</p>
+        <p><strong>Escolaridade : </strong> Ensino Superior incompleto</p>
+         <p><strong>Telefone : </strong> 16 996789685</p>
       </q-card-section>
 
       <q-separator />
-
-      <q-card-actions row justify-around>
+      
+      <div class="row justify-around btns">
         <q-btn unelevated
               rounded
-              class="q-px-xl btn"
-              label="Excluir conta"
+              class="btn"
+              label="Excluir"
               type="submit"
               color="red"
               icon="delete"/>
         <q-btn unelevated
               rounded
-              class="q-px-xl btn"
-              label="Editar dados"
+              class="btn"
+              label="Editar"
               type="submit"
               color="primary"
               icon="edit"
               />
-      </q-card-actions>
+      </div>
     </q-card>
+    
 </q-page>
 </template>
 <style>
-
+.btns{
+  padding: 14px;
+}
 </style>
